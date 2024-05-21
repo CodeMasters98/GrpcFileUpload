90ceb4fd8c1163eb153be8ed63212611b47625e8
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2024-05-21 16:17:42 +0330

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Google.Protobuf;
+using Grpc.Net.Client;
+using GrpcService.Protos;
+using System.Reflection;
+
+Console.WriteLine("press a key to start");
+Console.ReadKey();
+Console.Clear();
+
+var channel = GrpcChannel.ForAddress("https://localhost:7002");
+var client = new FileStreamingService.FileStreamingServiceClient(channel);
+
+var contentRootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+string file = Path.Combine(contentRootPath, "Files", "sample.mp4");
+var fileInfo = new FileInfo(file);
+
+decimal chunkSize = 0;
+var buffer = new byte[1024 * 2];
+using var call = client.UploadFile();
+using var fileStream = new FileStream(file, FileMode.Open);
+var content = new ByteContent
+{
+    FileSize = fileStream.Length,
+    ReadedByte = 0,
+    Info = new Info { FileName = Path.GetFileNameWithoutExtension(fileInfo.Name), FileExtension = fileInfo.Extension }
+};
+while ((content.ReadedByte = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+{
+    content.Buffer=ByteString.CopyFrom(buffer);
+    await call.RequestStream.WriteAsync(content);
+    chunkSize += buffer.Length;
+    Console.WriteLine($"{Math.Round(chunkSize * 100 / fileStream.Length)} %");
+    await Task.Delay(100);
+}
+await call.RequestStream.CompleteAsync();
+Console.ReadKey();
---
+++ GrpcService/Services/GrpcFileStreamingService.cs
@@ -0,0 +1,43 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcService.Protos;
+
+namespace GrpcService.Services
+{
+    public class GrpcFileStreamingService : FileStreamingService.FileStreamingServiceBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private string uploadPath;
+        public GrpcFileStreamingService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            uploadPath = Path.Combine(_webHostEnvironment.ContentRootPath, "files");
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+        }
+        public override async Task<Empty> UploadFile(IAsyncStreamReader<ByteContent> requestStream, ServerCallContext context)
+        {
+            FileStream fileStream = null;
+            decimal chunkSize = 0;
+            bool init = true;
+            await foreach (var byteContent in requestStream.ReadAllAsync())
+            {
+                if (init)
+                {
+                    fileStream = new FileStream($"{uploadPath}/{byteContent.Info.FileName}{byteContent.Info.FileExtension}",FileMode.OpenOrCreate,FileAccess.ReadWrite,FileShare.None);
+
+                }
+                var buffer = byteContent.Buffer.ToByteArray();
+                await fileStream.WriteAsync(buffer,0, buffer.Length);
+                chunkSize += byteContent.ReadedByte;
+                Console.WriteLine($"{Math.Round(chunkSize*100/byteContent.FileSize)} %");
+                init= false;
+            }
+            await fileStream.DisposeAsync();
+            fileStream.Close();
+            return new Empty();
+        }
+    }
+}
